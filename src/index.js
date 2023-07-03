9501fa7c2d860fcc28d4e261c5f6a4686a4c22b9
Jesus Marron <jesusmarron7@gmail.com>
2023-07-03 10:43:12 -0500

    calculateTotal implemented. Tests passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 export function calculateTotal (items, tax) {
-  // TODO
+  
+  tax = Math.abs(tax);
+
+
+  if (tax > 1) {
+    throw new Error("Tax must be entered as a percentage, not whole number!")
+  }
+  
+  let total = 0;
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    total += item.price;
+
+    if (item.taxable) {
+      total += item.price * tax;
+    }
+  }
+
+  return total;
+
 }
